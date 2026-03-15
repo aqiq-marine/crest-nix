bc28f82dc6fc922a202d449a2e3778d0af138d2a
aqua <aoi.kiyohara0306@gmail.com>
2026-03-15 21:56:19 +0900

    fix: add some dependancy

--- flake.nix
+++ flake.nix
@@ -1,92 +1,148 @@
 {
-  description = "CREST + xTB build";
+  description = "CREST + xTB (fully vendored)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
 
-        xtb = pkgs.stdenv.mkDerivation {
-          pname = "xtb";
-          version = "6.7";
+    commonNative = with pkgs; [ cmake gfortran ];
+    linalg = with pkgs; [ openblas lapack ];
 
-          src = pkgs.fetchFromGitHub {
-            owner = "grimme-lab";
-            repo = "xtb";
-            rev = "v6.7.1";
-            sha256 = "sha256-+qgXSMwzD0xSycZIRTokt77fZHHZQ++Npzr7NLlypOA=";
-          };
+    mctc-lib = pkgs.stdenv.mkDerivation {
+      pname = "mctc-lib";
+      version = "0.3.1";
 
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.gfortran
-          ];
+      src = pkgs.fetchFromGitHub {
+        owner = "grimme-lab";
+        repo = "mctc-lib";
+        rev = "v0.3.1";
+        sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      };
 
-          buildInputs = [
-            pkgs.openblas
-            pkgs.lapack
-          ];
+      nativeBuildInputs = commonNative;
 
-          cmakeFlags = [
-            "-DCMAKE_BUILD_TYPE=Release"
-          ];
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
+      ];
+    };
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/xtb $out/bin/
-          '';
-        };
+    multicharge = pkgs.stdenv.mkDerivation {
+      pname = "multicharge";
+      version = "0.3.0";
 
-        crest = pkgs.stdenv.mkDerivation {
-          pname = "crest";
-          version = "3.0.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "grimme-lab";
+        repo = "multicharge";
+        rev = "v0.3.0";
+        sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
+      };
 
-          src = pkgs.fetchFromGitHub {
-            owner = "crest-lab";
-            repo = "crest";
-            rev = "v3.0.2";
-            sha256 = "sha256-AVLCC5banxmBQX8tuN2zSQbM7wKwrymfXLT5MBQSpPY=";
-          };
+      nativeBuildInputs = commonNative;
 
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.gfortran
-          ];
+      buildInputs = [ mctc-lib ];
 
-          buildInputs = [
-            pkgs.openblas
-            pkgs.lapack
-            xtb
-          ];
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
+      ];
+    };
 
-          cmakeFlags = [
-            "-DCMAKE_BUILD_TYPE=Release"
-          ];
+    tblite = pkgs.stdenv.mkDerivation {
+      pname = "tblite";
+      version = "0.3.0";
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp _build/crest $out/bin/
-          '';
-        };
+      src = pkgs.fetchFromGitHub {
+        owner = "tblite";
+        repo = "tblite";
+        rev = "v0.3.0";
+        sha256 = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
+      };
 
-      in
-      {
-        packages.default = crest;
+      nativeBuildInputs = commonNative;
 
-        packages = {
-          inherit crest xtb;
-        };
+      buildInputs = [
+        mctc-lib
+        multicharge
+      ] ++ linalg;
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            crest
-            xtb
-          ];
-        };
-      });
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
+      ];
+    };
+
+    xtb = pkgs.stdenv.mkDerivation {
+      pname = "xtb";
+      version = "6.7.1";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "grimme-lab";
+        repo = "xtb";
+        rev = "v6.7.1";
+        sha256 = "sha256-+qgXSMwzD0xSycZIRTokt77fZHHZQ++Npzr7NLlypOA=";
+      };
+
+      nativeBuildInputs = commonNative;
+
+      buildInputs =
+        [ mctc-lib tblite multicharge ]
+        ++ linalg;
+
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
+        "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp xtb $out/bin/
+      '';
+    };
+
+    crest = pkgs.stdenv.mkDerivation {
+      pname = "crest";
+      version = "3.0.2";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "crest-lab";
+        repo = "crest";
+        rev = "v3.0.2";
+        sha256 = "sha256-AVLCC5banxmBQX8tuN2zSQbM7wKwrymfXLT5MBQSpPY=";
+      };
+
+      nativeBuildInputs = commonNative;
+
+      buildInputs =
+        [ xtb ]
+        ++ linalg;
+
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
+        "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp crest $out/bin/
+      '';
+    };
+
+  in
+  {
+    packages = {
+      inherit crest xtb tblite multicharge mctc-lib;
+      default = crest;
+    };
+
+    devShells.default = pkgs.mkShell {
+      packages = [
+        crest
+        xtb
+      ];
+    };
+  });
 }
