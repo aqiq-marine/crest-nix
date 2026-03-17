1a036588fb4ab24f5532ac9bdd49d756a59146a4
aqua <aoi.kiyohara0306@gmail.com>
2026-03-17 12:10:43 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
 
     tblite = pkgs.stdenv.mkDerivation rec {
       pname = "tblite";
-      version = "0.4.0";
+      version = "0.3.0";
 
       src = pkgs.fetchFromGitHub {
         owner = "tblite";
