c9b44384de617de519c0c82d5abdf2a458875f79
aqua <aoi.kiyohara0306@gmail.com>
2026-03-17 23:01:03 +0900

    tblite0.3.0, toml-f0.3.1

--- flake.nix
+++ flake.nix
@@ -135,7 +135,7 @@
         # sha256 = "sha256-KV2fxB+SF4LilN/87YCvxUt4wsY4YyIV4tqnn+3/0oI=";
         # 0.3.0
         sha256 = "sha256-R7CAFG/x55k5Ieslxeq+DWq1wPip4cI+Yvn1cBbeVNs=";
-        sha256 = pkgs.lib.fakeSha256;
+        # sha256 = pkgs.lib.fakeSha256;
       };
 
       nativeBuildInputs = [
